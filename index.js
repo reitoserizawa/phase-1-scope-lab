6debcb4c755d35a9bd938057b291fe6daf35d8f8
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-05 23:03:17 -0400

    commit tree 02a5a053bd85a8023f63411259a8b30147c9af68

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase()
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    bestCustomer
+}
+
+const leastFavoriteCustomer = 'value'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'me'
+    bestCustomer
+}
